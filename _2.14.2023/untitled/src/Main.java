6de043e3efb1de49ab1241d3f8204e4d0885c7f3
IAIvanov19 <IAIvanov19@codingburgas.bg>
2023-02-14 09:10:30 +0200

    exceptions

---
+++ _2.14.2023/untitled/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int i = 12;
+            int j = 0;
+            int result = i/j;
+            System.out.println(result);
+        } catch (NumberFormatException ex){
+            System.out.println(ex.getMessage() + "!!!");
+        } catch (ArithmeticException ex){
+            System.out.println(ex.getMessage() + "...");
+        } catch (Exception ex){
+            System.out.println(ex.getMessage() + ",,,,,");
+        }
+
+
+        System.out.println("test");
+    }
+}
