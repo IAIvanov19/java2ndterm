5233fa450440ec9f4ae769aa82b9540b943bf077
IAIvanov19 <IAIvanov19@codingburgas.bg>
2023-02-21 09:03:06 +0200

    vtori chas

--- _2.21.2023/src/Main.java
+++ _2.21.2023/src/Main.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\iaivanov19\\source\\repos\\random.txt";
-        FileInputStream inputStream = new FileInputStream(path);
-        String outputPath = "C:\\Users\\iaivanov19\\source\\repos\\random.txt";
-        FileOutputStream outputStream = new FileOutputStream(outputPath);
-        int oneByte = inputStream.read();
-        while (oneByte >= 0) {
-            outputStream.write(oneByte);
-            oneByte = inputStream.read();
+        String path = "C:\\Users\\VRKirov19\\source\\repos\\txtFile.txt";
+        String outputPath = "C:\\Users\\VRKirov19\\source\\repos\\Output.txt"
+        FileReader fileReader = new FileReader(path);
+        BufferedReader reader = new BufferedReader(fileReader);
+        FileWriter fileWriter = new FileWriter(outputPath);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath));
+        String currentLine = reader.readLine();
+        while (currentLine != null) {
+            int sum = 0;
+            for (int i = 0; i < currentLine.length(); i++) {
+                sum += currentLine.charAt(i);
+            }
+            fileWriter.write(String.valueOf(sum));
+            writer.write("\n");
+            currentLine = reader.readLine();
         }
     }
 }
